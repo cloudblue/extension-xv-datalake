231d3bc36b7ba5b833402a2d312595f975c3f9b8
rmondal00 <rahul.mondal@ingrammicro.com>
2023-08-24 18:30:21 +0200

    LITE-28491 Include product id and locale id at the top of the schema

--- tests/api/test_tier_configs.py
+++ tests/api/test_tier_configs.py
@@ -11,6 +11,28 @@ from connect_ext_datalake.services.client import GooglePubsubClient
 from connect_ext_datalake.webapp import DatalakeExtensionWebApplication
 
 
+def test_publish_all_tc_info_success(
+    client_mocker_factory,
+    test_client_factory,
+    installation,
+    context,
+):
+    client_mocker = client_mocker_factory()
+
+    client_mocker('devops').services[context['extension_id']].environments[
+        context['environment_id']].schedules.create(return_value={})
+
+    client = test_client_factory(DatalakeExtensionWebApplication)
+
+    response = client.post(
+        '/api/tier/configs/*/publish-all',
+        installation=installation,
+        context=context,
+    )
+
+    assert response.status_code == 202
+
+
 def test_publish_all_tc_info_failed(
     client_mocker_factory,
     test_client_factory,
